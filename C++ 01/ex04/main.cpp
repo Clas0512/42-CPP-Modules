92557f8482da1fa0fb20528fe8a5783565ebf36c
Clas0512 <mrb.bn@hotmail.com>
2023-06-20 13:15:58 +0300

    makefile commit

--- C++ 01/ex04/main.cpp
+++ C++ 01/ex04/main.cpp
@@ -5,22 +5,48 @@
 int main(int ac, char **av){
 
     std::ifstream inputFile;
-    std::string filename;
+    std::ofstream outputFile;
+    std::string replaceFileName;
+    std::string fileName;
+    std::size_t found;
+    std::string temp;
     std::string s1;
     std::string s2;
     if (ac == 4)
     {
-        filename = av[1];
+        fileName = av[1];
+        replaceFileName = fileName + ".replace";
         s1 = av[2];
         s2 = av[3];
-        if (inputFile.is_open())
+        inputFile.open(fileName);
+        outputFile.open(replaceFileName);
+        if (!outputFile.is_open())
         {
-            while (std::getline(inputFile, line))
-            {
-
-            }
+            std::cout << "ERROR: file cannot open!" << std::endl;
+            return 0;
         }
-
+        if (!inputFile.is_open())
+        {
+            std::cout << "ERROR: file cannot open!" << std::endl;
+            return 0;
+        }
+        while (std::getline(inputFile, temp))
+        {
+            std::cout << temp << std::endl;
+        }
+        inputFile.close();
+        inputFile.open(fileName);
+        temp = "";
+        while (std::getline(inputFile, temp))
+        {
+            found = temp.find(s1);
+            while (found != std::string::npos)
+            {
+                temp.replace(found, s1.length(), s2);
+                found = temp.find(s1);
+            }
+            outputFile << temp << std::endl;
+        }
     }
     return 0;
 }
