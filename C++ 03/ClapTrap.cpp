6279874f415f1a34e65b1b491bf64f2c840634b9
Adem berke Nargul <anargul@k1m32s04.42istanbul.com.tr>
2023-06-22 15:13:39 +0300

    Changes in C++ 03/ex00

--- C++ 03/ClapTrap.cpp
+++ C++ 03/ClapTrap.cpp
@@ -1,26 +1,79 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap(name)
+ClapTrap::ClapTrap(void)
 {
-	Name = name;
+    static int a;
+    std::stringstream b;
+    std::string d;
+    b << a;
+    b >> d;
+    this->name = d;
+    this->hitPoints = 10;
+    this->attackDamage = 0;
+    this->energyPoints = 10;
+    std::cout << "ClapTrap Constructor Start the <" << name << ">" << std::endl;
 }
 
-ClapTrap::~ClapTrap()
+ClapTrap::ClapTrap(std::string name)
 {
-	
+    this->name = name;
+    this->hitPoints = 10;
+    this->attackDamage = 0;
+    this->energyPoints = 10;
+    std::cout << "ClapTrap Constructor Start the <" << name << ">" << std::endl;
 }
 
 void	ClapTrap::attack(const std::string &target)
 {
-	target.takeDamage(this->attackDamage);
+    if (this->hitPoints > 0 && this->energyPoints > 0)
+    {
+        std::cout << "ClapTrap <" << this->name << "> attacks <" << target << ">, causing <" << this->attackDamage << "> points of damage!" << std::endl;
+        this->energyPoints--;
+    }
+    else if (this->hitPoints <= 0)
+        std::cout << "ClapTrap <" << this->name << "> cannot attack because it's already dead." << std::endl;
+    else if (this->energyPoints <= 0)
+        std::cout << "ClapTrap <" << this->name << "> cannot attack because it doesn't have energy point enough!" << std::endl;
 }
 
 void	ClapTrap::takeDamage(unsigned int amount)
 {
-	
+	std::cout << "ClapTrap <" << name << "> takes <" << amount << "> points of damage!" << std::endl;
+    if (amount - hitPoints >= 0)
+        this->hitPoints -= amount;
+    else
+    {
+        this->hitPoints = 0;
+        std::cout << "ClapTrap died!" << std::endl;
+    }
 }
 
 void	ClapTrap::beRepaired(unsigned int amount)
 {
-	
+    if (this->hitPoints > 0 && this->energyPoints > 0)
+    {
+        if (this->hitPoints + amount > 10)
+            this->hitPoints = 10;
+        else
+            this->hitPoints += amount;
+    }
+    else if (this->hitPoints <= 0)
+        std::cout << "ClapTrap <" << this->name << "> cannot repair because it's already dead." << std::endl;
+    else if (this->energyPoints <= 0)
+        std::cout << "ClapTrap <" << this->name << "> cannot repair because it doesn't have energy point enough!" << std::endl;
+}
+
+std::string ClapTrap::getName()
+{
+    return this->name;
+}
+
+int ClapTrap::getAttackDamage()
+{
+    return this->attackDamage;
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << "ClapTrap Destructor destroy the <" << name << ">" << std::endl;
 }
--- C++ 03/ClapTrap.hpp
+++ C++ 03/ClapTrap.hpp
@@ -2,18 +2,22 @@
 #define CLAPTRAP_HPP
 
 #include <iostream>
+#include <sstream>
 
 class ClapTrap
 {
 public:
-	ClapTrap(name);
+	ClapTrap();
+	ClapTrap(std::string name);
+	~ClapTrap();
+	int getAttackDamage();
+	std::string getName();
 	void attack(const std::string &target);
 	void takeDamage(unsigned int amount);
 	void beRepaired(unsigned int amount);
-	~ClapTrap();
 
 private:
-	std::string Name;
+	std::string name;
 	int hitPoints;
 	int energyPoints;
 	int attackDamage;
