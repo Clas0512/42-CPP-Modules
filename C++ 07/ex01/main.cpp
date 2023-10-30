05e8b7fc80da1dac78b657bf85d3b452760064f8
Adem berke Nargul <anargul@k1m33s09.42istanbul.com.tr>
2023-10-30 13:33:54 +0300

    some fixes

--- C++ 07/ex01/main.cpp
+++ C++ 07/ex01/main.cpp
@@ -1,8 +1,5 @@
 #include "iter.hpp"
 
-# include <iostream>
-
-
 void printInteger(const int &a)   
 {
     std::cout << a << std::endl;
@@ -13,11 +10,7 @@ void sumWithOne(int &a)
     a += 1;
 }
 
-template<typename I, typename F, typename Z>
-void iter(I *iArray, size_t length, F function(Z &element)){
-    for (size_t i = 0; i < length; i++)
-        function(iArray[i]);
-}
+
 int main(void)
 {
     int array[5] = {1, 2, 3, 4, 5};
