39d4932ce7c3c16ac6bc12310fb27b6cdd045ea7
Adem berke Nargul <anargul@k1m33s09.42istanbul.com.tr>
2023-10-30 09:04:35 +0300

    ex02 is done

---
+++ C++ 07/ex02/Array.hpp
@@ -0,0 +1,61 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+
+template<typename T>
+class Array
+{
+	private:
+        T* mainArray;
+        unsigned int arraySize;
+	public:
+		Array(void) {
+			mainArray = new T[0];
+			arraySize = 0;
+		}
+		~Array(void) {
+			std::cout << "Default Destructor Call" << std::endl;
+    		delete[] mainArray;
+		}
+        Array(unsigned int n)
+		{
+			arraySize = n;
+			if (n > 0)
+				mainArray = new T[n];
+			else
+				mainArray = nullptr;
+		}
+		Array(const Array &copy){
+			*this = copy;
+		}
+		Array &operator=(const Array &rhs) {
+    		if (this != &rhs) {
+        		arraySize = rhs.arraySize;
+        		if (arraySize > 0) {
+            		mainArray = new T[arraySize];
+            		for (unsigned int i = 0; i < arraySize; ++i)
+                		mainArray[i] = rhs.mainArray[i];
+        		} 
+				else
+            		mainArray = nullptr;
+    		}
+    		return *this;
+		}
+		class outOfBound : public std::exception{
+			public:
+				virtual const char* what() const throw(){
+					return ("Out of bound!");
+				};
+		};
+		T& operator[](unsigned int n) {
+    		if (n >= this->arraySize || n < 0)
+				throw outOfBound();
+    		return (mainArray[n]);
+		}
+		unsigned int size(void) {
+			return arraySize;
+		}
+};
+
+#endif
