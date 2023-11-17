be558ff4bcbbbcba73b145d706b0d5ac84a36503
Adem berke Nargül <clas0512@Adems-MacBook-Pro.local>
2023-11-17 18:48:00 +0300

    Cpp09 processing again

--- C++ 09/ex000/BitcoinExchange.hpp
+++ C++ 09/ex000/BitcoinExchange.hpp
@@ -6,16 +6,41 @@
 # include <fstream>
 # include <sstream>
 
+# define CSVFILENAME "data.csv"
+
 class BitcoinExchange
 {
 	private:
-        std::map<std::string, int> map00;
+		std::string						csvFileName;
+		std::string						inputFileName;
+		std::ifstream					csvFile;
+		std::ifstream					inputFile;
+        std::map<int *, float>	mapCsv;
 	public:
+		BitcoinExchange(std::string fileName);
 		BitcoinExchange(void);
-		BitcoinExchange(** replace parameters **);
 		BitcoinExchange(BitcoinExchange const &instance);
 		BitcoinExchange &operator=(BitcoinExchange const &rhs);
 		~BitcoinExchange(void);
+		std::map<int *, float>	locateCsvInMap();
+		std::map<int *, float>	&getMap();
+
+		class MyException : public std::exception
+		{
+			private:
+				const char* errorString;
+			public:
+				MyException(const char *str) : errorString(str){};
+			const char* what() const throw()
+			{
+				return (errorString);
+			}
+		};
 };
+
+int *stringDateToInt(std::string stringDate);
+std::ifstream	openFile(std::string fileName);
+std::ifstream	openFile(void);
+void	calculateBitcoins();
 
 #endif
--- C++ 09/ex000/utilities.cpp
+++ C++ 09/ex000/utilities.cpp
@@ -1,2 +1,36 @@
 #include "BitcoinExchange.hpp"
 
+std::ifstream	openFile()
+{
+    std::ifstream returnFile;
+    returnFile.open(CSVFILENAME, std::ios::in);
+    if (!returnFile.is_open())
+        throw std::invalid_argument("selam dostum yağmur var mi orda?");
+    return (returnFile);
+}
+
+std::ifstream	openFile(std::string fileName)
+{
+    std::ifstream returnFile;
+    returnFile.open(fileName, std::ios::in);
+    if (!returnFile.is_open())
+        throw std::invalid_argument("selam dostum yağmur var mi orda?");
+    return (returnFile);
+}
+
+int *stringDateToInt(std::string stringDate)
+{
+    int *integerDate;
+
+    integerDate = new int[3];
+    std::istringstream ss(stringDate);
+    std::string dateElements[3];
+
+    std::getline(ss, dateElements[0], '-');
+    integerDate[0] = std::stoi(dateElements[0]);
+    std::getline(ss, dateElements[1], '-');
+    integerDate[1] = std::stoi(dateElements[1]);
+    std::getline(ss, dateElements[2], '-');
+    integerDate[2] = std::stoi(dateElements[2]);
+    return (integerDate);
+}
